79fc28461ca355748dd9ec5dc9dd6970609ee070
gustavohenriqdev <gustavohen.dev@gmail.com>
2023-12-21 13:23:47 -0300

    Criacao do arquivo app.js, adicionando interacao com JavaScript

---
+++ app.js
@@ -0,0 +1,7 @@
+let elementoDuvida = document.querySelectorAll('.duvida');
+
+elementoDuvida.forEach(function (duvida) {
+    duvida.addEventListener('click', function() {
+        duvida.classList.toggle('active')
+    })
+});
